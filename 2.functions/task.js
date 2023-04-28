9f114e4ae5a1a765db587dac2a8853322e9b5aed
Alexander Kozhin <077hustler@gmail.com>
2023-04-29 01:29:43 +0300

    Задание 2 в работе

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,17 +1,49 @@
 function getArrayParams(...arr) {
+	let min = arr[0];
+	let max = arr[0];
+	let sum = 0;
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] > max) {
+			max = arr[i];
+		} else if (arr[i] < min) {
+			min = arr[i];
+		}
+		sum += arr[i];
+	}
+	let avg = (+(sum / arr.length).toFixed(2));
 
-  return { min: min, max: max, avg: avg };
+	return { min: min, max: max, avg: avg };
 }
 
 function summElementsWorker(...arr) {
-
+	let sum = 0;
+	for (let i = 0; i < arr.length; i++) {
+		sum += arr[i];
+	}
+	return (sum);
 }
 
 function differenceMaxMinWorker(...arr) {
-
+	let max = Math.max(arr);
+	let min = Math.min(arr);
+	let difference = max - min;
+	return (difference);
 }
 
 function differenceEvenOddWorker(...arr) {
+	let sumEvenElement = 0;
+	let sumOddElement = 0;
+	let difference = sumEvenElement - sumOddElement;
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] % 2 === 0) {
+			sumEvenElement += arr[i];
+		} else {
+			sumOddElement += arr[i];
+		}
+	}
+	return (difference);
+
+
 
 }
 
@@ -19,6 +51,6 @@ function averageEvenElementsWorker(...arr) {
 
 }
 
-function makeWork (arrOfArr, func) {
+function makeWork(arrOfArr, func) {
 
 }
